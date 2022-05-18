f60ff73d35f6da629aeb60928f4cd8172e43f563
Arman Matinyan <a.a.matinyan@gmail.com>
2022-05-18 23:06:56 +0400

    Initial commit

---
+++ game-engine/game-engine.cpp
@@ -0,0 +1,12 @@
+﻿// game-engine.cpp : Defines the entry point for the application.
+//
+
+#include "game-engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
